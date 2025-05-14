c34e55d7dccedf8b2534f26e02d99d0616d92559
JongBin Noh <95959567+begong313@users.noreply.github.com>
2025-05-14 14:01:34 +0900

    기본 생성자 사용 및 UI 요소 추가: const 키워드를 사용하여 성능 최적화, 총 잔액 및 이체 버튼 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF181818),
+        backgroundColor: const Color(0xFF181818),
         body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 40),
+          padding: const EdgeInsets.symmetric(horizontal: 40),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 80),
+              const SizedBox(height: 80),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Text(
+                      const Text(
                         "hey, jb",
                         style: TextStyle(
                           color: Colors.white,
@@ -37,6 +40,41 @@ class App extends StatelessWidget {
                         ),
                       ),
                     ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 120),
+              Text(
+                "Total Balance",
+                style: TextStyle(
+                  color: Colors.white.withValues(alpha: 0.8),
+                  fontSize: 22,
+                ),
+              ),
+              const SizedBox(height: 15),
+              const Text(
+                "\$1 123 456",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 44,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(height: 30),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 50,
+                      ),
+                      child: Text('Transfer', style: TextStyle(fontSize: 22)),
+                    ),
                   ),
                 ],
               ),
